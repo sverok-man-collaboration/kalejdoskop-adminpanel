86e87b3d9c89fa927661db999a06ff50933d7f5b
SofiaDrejmark <sofiadrejmark@gmail.com>
2023-02-15 09:57:10 +0100

    first commit. installed react-vite, jsdoc, axios

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Admin</h1>
+    </div>
+  )
+}
+
+export default App
